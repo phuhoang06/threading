acbe93c5db02a129df8f7dc04c62c13df6f3e78b
phuhoang.06 <hoangvanphu2006@gmail.com>
2024-11-28 09:01:32 +0700

    th

--- src/th1/Main.java
+++ src/th1/Main.java
@@ -1,6 +1,8 @@
+package th1;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main thread running... ");
+        System.out.println("th1.Main thread running... ");
 
         RunnableDemo runnableDemo1 = new RunnableDemo("Thread-1-HR-Database");
         runnableDemo1.start();
@@ -8,6 +10,6 @@ public class Main {
         RunnableDemo runnableDemo2 = new RunnableDemo("Thread-2-Send-Email");
         runnableDemo2.start();
 
-        System.out.println("Main thread stopped!!! ");
+        System.out.println("th1.Main thread stopped!!! ");
     }
 }
--- src/th1/RunnableDemo.java
+++ src/th1/RunnableDemo.java
@@ -1,3 +1,5 @@
+package th1;
+
 public class RunnableDemo implements Runnable {
     private Thread t;
     private final String threadName;
